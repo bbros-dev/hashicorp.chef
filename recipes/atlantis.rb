f0c17eb6f73cc35b41c677ebc9496b720b72a77e
Mark Van de Vyver <mark@taqtiqa.com>
2020-10-17 22:47:39 +1100

    [Add][Infra] Drone & Atlantis
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

--- recipes/atlantis.rb
+++ recipes/atlantis.rb
@@ -17,13 +17,14 @@ ark 'Install Atlantis binary' do
   has_binaries [hashicorp.atlantis_name]
   mode '0755'
   name hashicorp.atlantis_name
+  not_if "[[ $(atlantis version) == *#{hashicorp.atlantis_version}* ]]"
   prefix_root hashicorp.bin_root
   strip_components 0
   url "https://github.com/runatlantis/atlantis/releases/download/#{hashicorp.atlantis_version}/atlantis_linux_#{hashicorp.install_arch}.zip"
   version hashicorp.atlantis_version
 end
 
-## Install Tunnelto Binary
+## Install TunnelTo Binary
 #
 # This uses a Rust binary, the async by default should ensure good performance.
 #
@@ -34,6 +35,7 @@ ark 'Install TunnelTo binary' do
   has_binaries [hashicorp.tunnelto_name]
   mode '0755'
   name hashicorp.tunnelto_name
+  not_if "[[ $(tunnelto --version) == *#{hashicorp.tunnelto_version}* ]]"
   prefix_root hashicorp.bin_root
   strip_components 0
   url "https://github.com/agrinman/tunnelto/releases/download/#{hashicorp.tunnelto_version}/tunnelto-linux.tar.gz"
